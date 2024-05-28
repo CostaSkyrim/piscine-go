85c67679f7576a464b421cff439f5836cd2ce311
Konstantinos Petroutsos <kpetrout@noreply.platform.zone01.gr>
2024-05-28 16:53:52 +0000

    Upload files to ''

---
+++ myfamily.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+
+relatives=$(curl -s "https://platform.zone01.gr/assets/superhero/all.json" | jq '.[] | select(.id == ($HERO_ID | tonumber)) | .connections.relatives')
+relatives=$(echo "$relatives" | sed 's/"//g')
+echo "$relatives"
