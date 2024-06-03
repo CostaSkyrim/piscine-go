e150d2fd53e479489dab1b8d62f02073e035792e
kpetrout <kostas.petroutsos@gmail.com>
2024-06-03 18:30:50 +0300

    iterativefactorial #3

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,8 +2,14 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	for i := 1; i <= nb; i++ {
-		result = result * i
+	if nb > 0 {
+		for i := 1; i <= nb; i++ {
+			result = result * i
+		}
+	} else if nb == 0 {
+		result = 1
+	} else {
+		result = 0
 	}
 	return result
 }
