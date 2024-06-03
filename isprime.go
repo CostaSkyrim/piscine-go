68b7aac1187d71b56ec17feef217eef668ea129f
kpetrout <kostas.petroutsos@gmail.com>
2024-06-04 02:33:55 +0300

    isprime #3

--- isprime.go
+++ isprime.go
@@ -1,19 +1,35 @@
 package piscine
 
-import "math"
-
 func IsPrime(nb int) bool {
 	if nb <= 1 {
 		return false
 	}
+	if nb == 2 {
+		return true
+	}
 	if nb%2 == 0 {
 		return false
 	}
-	sqrtN := int(math.Sqrt(float64(nb)))
+	sqrtN := intSqrt(nb)
 	for i := 3; i <= sqrtN; i += 2 {
 		if nb%i == 0 {
 			return false
 		}
 	}
 	return true
+}
+
+func intSqrt(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+	if nb == 0 || nb == 1 {
+		return nb
+	}
+	x, y := nb, (nb+1)/2
+	for y < x {
+		x = y
+		y = (nb/y + y) / 2
+	}
+	return x
 }
