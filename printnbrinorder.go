523986162e5a13370410c551ec9af2cf5d860967
kpetrout <kostas.petroutsos@gmail.com>
2024-06-05 00:09:08 +0300

    printnbrinorder

---
+++ printnbrinorder.go
@@ -0,0 +1,36 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	if n < 0 {
+		return
+	}
+	if n == 0 {
+		z01.PrintRune('0')
+		z01.PrintRune('\n')
+		return
+	}
+	digits := []rune{}
+	for n > 0 {
+		digit := rune(n%10 + '0')
+		digits = append(digits, digit)
+		n /= 10
+	}
+	bubbleSort(digits)
+	for _, digit := range digits {
+		z01.PrintRune(digit)
+	}
+	z01.PrintRune('\n')
+}
+
+func bubbleSort(arr []rune) {
+	n := len(arr)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
