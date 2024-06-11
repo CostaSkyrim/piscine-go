af8470b751816d2d30d395fae8b81fac76319446
kpetrout <kostas.petroutsos@gmail.com>
2024-06-11 22:13:12 +0300

    boolean #3

--- boolean/main.go
+++ boolean/main.go
@@ -24,7 +24,7 @@ func isEven(nbr int) bool {
 func main() {
 	s := os.Args[1:]
 	EvenMsg := "I have an even number of arguments"
-	OddMsg := "I have a odd number of arguments"
+	OddMsg := "I have an odd number of arguments"
 
 	if isEven(len(s)) {
 		printStr(EvenMsg)
