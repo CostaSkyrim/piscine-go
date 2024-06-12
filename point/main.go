95fc9102d7dc4c64be7d2ab97c91ec74df738e93
kpetrout <kostas.petroutsos@gmail.com>
2024-06-12 22:00:07 +0300

    point #2

--- point/main.go
+++ point/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 type point struct {
 	x int
@@ -12,10 +12,41 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func printInt(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	digits := []rune{}
+	for n > 0 {
+		digits = append(digits, rune(n%10+'0'))
+		n /= 10
+	}
+	for i := len(digits) - 1; i >= 0; i-- {
+		z01.PrintRune(digits[i])
+	}
+}
+
 func main() {
 	points := &point{}
 
 	setPoint(points)
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	printInt(points.x)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	printInt(points.y)
+	z01.PrintRune('\n')
 }
