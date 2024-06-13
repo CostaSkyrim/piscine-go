a2f68512a46c7631d69e01e575408d4f7bbc97a6
kpetrout <kostas.petroutsos@gmail.com>
2024-06-13 23:41:17 +0300

    descendappendrange #1

---
+++ descendappendrange.go
@@ -0,0 +1,16 @@
+package piscine
+
+func DescendAppendRange(max, min int) []int {
+	if max <= min {
+		return nil
+	}
+
+	length := max - min
+	result := []int{}
+
+	for i := 0; i < length; i++ {
+		result = append(result, max-i)
+	}
+
+	return result
+}
