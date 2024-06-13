9d93c119f4f540693bac4c4fa2620ccf97975689
kpetrout <kostas.petroutsos@gmail.com>
2024-06-13 17:20:53 +0300

    shoppingsummarycounter #2

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,17 +1,32 @@
 package piscine
 
-import "strings"
-
 func ShoppingSummaryCounter(str string) map[string]int {
 	// Initialize a map to store the count of each item
 	itemCount := make(map[string]int)
 
-	// Split the string into words (items) based on spaces
-	items := strings.Fields(str)
+	// Variable to hold the current item
+	var currentItem string
 
-	// Iterate over the items and count each one
-	for _, item := range items {
-		itemCount[item]++
+	// Iterate over each character in the string
+	for i := 0; i < len(str); i++ {
+		char := str[i]
+
+		// If the character is a space, it means we've reached the end of an item
+		if char == ' ' {
+			// If currentItem is not empty, add it to the map
+			if currentItem != "" {
+				itemCount[currentItem]++
+				currentItem = ""
+			}
+		} else {
+			// Otherwise, keep building the current item
+			currentItem += string(char)
+		}
+	}
+
+	// Add the last item to the map if there's any
+	if currentItem != "" {
+		itemCount[currentItem]++
 	}
 
 	return itemCount
