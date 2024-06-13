0a90263a4a512c7326736a9dc791aa2f75234d35
kpetrout <kostas.petroutsos@gmail.com>
2024-06-13 17:28:18 +0300

    shoppingsummarycounter #4

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -6,6 +6,7 @@ func ShoppingSummaryCounter(str string) map[string]int {
 
 	// Variable to hold the current item
 	var currentItem string
+	spaceCount := 0
 
 	// Iterate over each character in the string
 	for i := 0; i < len(str); i++ {
@@ -13,12 +14,18 @@ func ShoppingSummaryCounter(str string) map[string]int {
 
 		// If the character is a space, it means we've reached the end of an item
 		if char == ' ' {
+			spaceCount++
 			// If currentItem is not empty, add it to the map
 			if currentItem != "" {
 				itemCount[currentItem]++
 				currentItem = ""
 			}
 		} else {
+			// Add spaces to the map before starting a new item
+			if spaceCount > 0 {
+				itemCount[" "] += spaceCount
+				spaceCount = 0
+			}
 			// Otherwise, keep building the current item
 			currentItem += string(char)
 		}
@@ -28,5 +35,10 @@ func ShoppingSummaryCounter(str string) map[string]int {
 	if currentItem != "" {
 		itemCount[currentItem]++
 	}
+	// Add any trailing spaces to the map
+	if spaceCount > 0 {
+		itemCount[" "] += spaceCount
+	}
+
 	return itemCount
 }
