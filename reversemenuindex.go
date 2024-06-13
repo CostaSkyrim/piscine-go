ff853f4b99079886c01c9564e305d9a57ada39ce
kpetrout <kostas.petroutsos@gmail.com>
2024-06-14 02:39:47 +0300

    reversemenuindex #1

---
+++ reversemenuindex.go
@@ -0,0 +1,10 @@
+package piscine
+
+func ReverseMenuIndex(menu []string) []string {
+	length := len(menu)
+    	reversedMenu := make([]string, length)
+    	for i := range menu {
+        	reversedMenu[length-1-i] = menu[i]
+    	}
+    	return reversedMenu
+}
