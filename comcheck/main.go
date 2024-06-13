e404b4b1ffd135c5d650bcf00f1a6586b08529ee
kpetrout <kostas.petroutsos@gmail.com>
2024-06-14 01:47:00 +0300

    comcheck #2

--- comcheck/main.go
+++ comcheck/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	for i := 0; i <= len(os.Args[1:]); i ++ {
+	for i := 0; i <= len(os.Args[1:]); i++ {
 		if os.Args[i] == "galaxy" || os.Args[i] == "01" || os.Args[i] == "galaxy 01" {
 			fmt.Println("Alert!!!")
 		}
