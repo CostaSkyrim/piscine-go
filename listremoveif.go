5183d6bbcfe6633a09dc4f1d62b575e135a38231
kpetrout <kostas.petroutsos@gmail.com>
2024-06-19 22:32:25 +0300

    listremoveif.go

--- listremoveif.go
+++ listremoveif.go
@@ -1,13 +1,23 @@
 package piscine
 
 func ListRemoveIf(l *List, data_ref interface{}) {
+	if l == nil || l.Head == nil {
+		return
+	}
+
+	// Remove elements from the beginning of the list
+	for l.Head != nil && l.Head.Data == data_ref {
+		l.Head = l.Head.Next
+	}
+
 	current := l.Head
 
-	for current != nil {
-		if CompStr(current.Data, data_ref) {
-			next := current.Next
-			current.Next = nil
-			current = next
+	// Traverse the list and remove matching elements
+	for current != nil && current.Next != nil {
+		if current.Next.Data == data_ref {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
 		}
 	}
 }
