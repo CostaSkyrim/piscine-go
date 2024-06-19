b6f6f08b0cd01ffc9372eaa5aec5ccd8ff9d76ab
kpetrout <kostas.petroutsos@gmail.com>
2024-06-19 21:44:01 +0300

    listremoveif.go

---
+++ listremoveif.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ListRemoveIf(l *List, data_ref interface{}) {
+	current := l.Head
+
+	for current != nil {
+		if CompStr(current.Data, data_ref) && current != nil {
+			next := current.Next
+			current.Next = nil
+			current = next
+		}
+	}
+}
