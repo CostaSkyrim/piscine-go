00c2ed848a0b4aa2ffa3d861092d7bd66ac2f6c7
kpetrout <kostas.petroutsos@gmail.com>
2024-06-19 21:45:29 +0300

    listremoveif.go #2

--- listremoveif.go
+++ listremoveif.go
@@ -4,7 +4,7 @@ func ListRemoveIf(l *List, data_ref interface{}) {
 	current := l.Head
 
 	for current != nil {
-		if CompStr(current.Data, data_ref) && current != nil {
+		if CompStr(current.Data, data_ref) {
 			next := current.Next
 			current.Next = nil
 			current = next
